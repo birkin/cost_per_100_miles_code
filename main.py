859b12dc3c29755d85ee6bd340e891739de088f2
Birkin James Diana <birkin.diana@gmail.com>
2026-03-29 10:09:23 -0400

    Implements initial gas calculations

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 import logging
 from decimal import Decimal
 
-from lib.calculate_cents_per_kwh import get_ev_100_mile_cost_values, get_rounded_cents_per_kwh
+from lib.calculate_cents_per_kwh import get_ev_100_mile_cost_values, get_gas_100_mile_cost_values, get_rounded_cents_per_kwh
 
 
 def main() -> None:
@@ -18,10 +18,14 @@ def main() -> None:
     logging.basicConfig(level=logging.DEBUG, format='%(message)s')
     rhode_island_cents_per_kwh = get_rounded_cents_per_kwh()
     ev_100_mile_cost_values: list[tuple[str, Decimal]] = get_ev_100_mile_cost_values(rhode_island_cents_per_kwh)
+    gas_100_mile_cost_values: list[tuple[str, Decimal]] = get_gas_100_mile_cost_values()
 
     for efficiency_label, cost_per_100_miles in ev_100_mile_cost_values:
         dollars_per_100_miles = cost_per_100_miles / Decimal('100')
         logging.debug('%s: $%.2f per 100 miles', efficiency_label, dollars_per_100_miles)
+
+    for efficiency_label, cost_per_100_miles in gas_100_mile_cost_values:
+        logging.debug('%s: $%.2f per 100 miles', efficiency_label, cost_per_100_miles)
 
 
 if __name__ == '__main__':
